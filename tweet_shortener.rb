62e4b8186f8fb338b787e834bd9832b62a87d0ba
Emily Bolten <emilybolten@gmail.com>
2019-05-10 18:46:20 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+def dictionary
+  dictionary = {"hello" => 'hi', "to, two, too" => '2',
+  "for, four" => '4', 'be' => 'b', 'you' => 'u', "at" => "@", "and" => "&"}
+
+  return dictionary
+end
+
+def word_substituter(tweet)
+end
+
+def bulk_tweet_shortener(tweet)
+end
+
+def selective_tweet_shortener(tweet)
+end
+
+def shortened_tweet_truncator(tweet)
+end
+
