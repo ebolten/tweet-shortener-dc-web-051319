471ae5962a5dbf2db283cedd382b2b800b0182d7
Emily Bolten <emilybolten@gmail.com>
2019-05-10 18:47:52 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,7 +1,7 @@
 # Write your code here.
 def dictionary
-  dictionary = {"hello" => 'hi', "to, two, too" => '2',
-  "for, four" => '4', 'be' => 'b', 'you' => 'u', "at" => "@", "and" => "&"}
+  dictionary = {"hello" => 'hi', "to" => '2', "too" => "2", "two" => "2",
+  "for" => '4', "four" => "4", 'be' => 'b', 'you' => 'u', "at" => "@", "and" => "&"}
 
   return dictionary
 end
